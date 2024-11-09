0d5253417366dad5b07648089a3941a60a0ce8f9
Holbiwan <sabrinapapeau@gmail.com>
2024-11-09 14:58:58 +0100

    Fix MapString

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -4,30 +4,22 @@ class User {
   int age;
   double height;
 
-  User({
-    required this.id,
-    required this.name,
-    required this.age,
-    required this.height,
-  });
+  User(
+      {required this.id,
+      required this.name,
+      required this.age,
+      required this.height});
 
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'name': name,
-        'age': age,
-        'height': height,
-      };
+  Map toJson() => {'id': id, 'name': name, 'age': age, 'height': height};
 
-  static User fromJson(Map<String, dynamic> userJson) {
-    return User(
-      id: userJson['id'],
-      name: userJson['name'],
-      age: userJson['age'],
-      height: userJson['height'],
-    );
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return new User(
+        id: userJson['id'],
+        name: userJson['name'],
+        age: userJson['age'],
+        height: userJson['height']);
   }
 
-  @override
   String toString() =>
-      "User(id : $id ,name: $name, age: $age, height: $height)";
+      "User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height})";
 }
