f0a89de80c831d6a5b02d62d77da4121903cd330
Holbiwan <sabrinapapeau@gmail.com>
2024-11-12 14:17:20 +0100

    Remove unecessary comments

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -1,22 +1,15 @@
 import 'dart:async';
 
-// Simulates async request to fetch user,failing after 2 sec.
 Future<String> fetchUser() => Future.delayed(
       const Duration(seconds: 2),
       () => throw 'Cannot locate user',
     );
 
-// Main function retrieves user with error handling.
 Future<void> getUser() async {
   try {
-    // Await response from fetchUser (which may throw an error).
     String userData = await fetchUser();
-    print('User data retrieved: $userData');
+    print(userData);
   } catch (error) {
-    // Clearer error message for better understanding.
-    print('Error caught while fetching user: $error');
-  } finally {
-    // Block to execute actions in all cases, if needed.
-    print('Fetch user process completed.');
+    print('Error: $error');
   }
 }
